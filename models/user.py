c6517ea948ece498e2febe8e7c97cb5f814a234e
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-10-14 11:21:22 +0100

    8. First User

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# class User that inherits from BaseModel
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ a class for users"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
