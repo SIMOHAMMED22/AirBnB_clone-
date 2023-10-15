e323722a0a3e9f488504c81d54b9ad4908dc04fe
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-10-15 22:22:24 +0100

    test case 1.0.2

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,19 +4,23 @@
 from models.base_model import BaseModel
 import unittest
 from datetime import datetime
-import uuid
+
 
 class TestBaseModel(unittest.TestCase):
-    
+    """ ### """
+
     def test_type_updated_at(self):
+        """ ### """
         b = BaseModel()
         self.assertEqual(type(b.updated_at), datetime)
         
     def test_type_created_at(self):
+        """ ### """
         b = BaseModel()
         self.assertEqual(type(b.created_at), datetime)
 
     def test_type_id(self):
+        """ ### """
         b = BaseModel()
         self.assertEqual(type(b.id), str)
 
