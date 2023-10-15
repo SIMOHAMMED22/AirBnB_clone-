cff85cc16ff0e66c3c4314839e2424d3bd0c36c6
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-10-15 22:29:06 +0100

    test cases 1.0.4

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,7 +4,17 @@ from models.engine.file_storage import FileStorage
 import unittest
 
 class TestFileStorage(unittest.TestCase):
-    ...
+    """ ### """
+
+    def test_file_path(self):
+        """ ### """
+        f = FileStorage()
+        self.assertEqual(f.__file_path, 'file.json')
+
+    def test_type_objects(self):
+        """ ### """
+        f = FileStorage()
+        self.assertEqual(f.__objects, {})
 
 if __name__ == '__main__':
     unittest.main()
