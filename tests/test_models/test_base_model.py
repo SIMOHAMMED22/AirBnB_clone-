65ad2fdf5223f234ab0bbfa6dfa1f188af074dde
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-10-15 22:09:30 +0100

    test case 1.0.1

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
 """Unit Test"""
+
 from models.base_model import BaseModel
 import unittest
+from datetime import datetime
+import uuid
 
 class TestBaseModel(unittest.TestCase):
-    ...
+    
+    def test_type_updated_at(self):
+        b = BaseModel()
+        self.assertEqual(type(b.updated_at), datetime)
+        
+    def test_type_created_at(self):
+        b = BaseModel()
+        self.assertEqual(type(b.created_at), datetime)
+
+    def test_type_id(self):
+        b = BaseModel()
+        self.assertEqual(type(b.id), str)
+
 
 if __name__ == '__main__':
     unittest.main()
