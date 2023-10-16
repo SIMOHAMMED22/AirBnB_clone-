e209ac9d7ae52f84a17df1af1a12bf0af5b0fd76
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-10-16 12:27:50 +0100

    test case 1.0.8

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -9,18 +9,33 @@ class TestUser(unittest.TestCase):
 
     def test_type_updated_at(self):
         """ ### """
-        b = User()
-        self.assertEqual(type(b.updated_at), datetime)
+        u = User()
+        self.assertEqual(type(u.updated_at), datetime)
         
     def test_type_created_at(self):
         """ ### """
-        b = User()
-        self.assertEqual(type(b.created_at), datetime)
+        u = User()
+        self.assertEqual(type(u.created_at), datetime)
 
     def test_type_id(self):
         """ ### """
-        b = User()
-        self.assertEqual(type(b.id), str)
+        u = User()
+        self.assertEqual(type(u.id), str)
+
+    def test_email(self):
+        """ ### """
+        u = User()
+        self.assertEqual(type(u.email), str)
+
+    def test_first_name(self):
+        """ ### """
+        u = User()
+        self.assertEqual(type(u.first_name), str)
+
+    def test_password(self):
+        """ ### """
+        u = User()
+        self.assertEqual(type(u.password), str)
 
 if __name__ == '__main__':
     unittest.main()
